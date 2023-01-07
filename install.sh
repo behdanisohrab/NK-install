491260d9f6189e52804615e8adfb95123e2b0e57
Sohrab Behdani <behdanisohrab@gmail.com>
2023-01-07 11:42:42 +0330

    init

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 echo "PLEASE WAIT....."
-echo "      "
-echo "      "
 
-cat .wf
+
+cat LOGO
 
 
 wget https://github.com/MatsuriDayo/nekoray/releases/download/2.9/nekoray-2.9-2022-12-19-linux64.zip
